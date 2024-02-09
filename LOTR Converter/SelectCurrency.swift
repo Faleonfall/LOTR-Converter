b0f3c65ade3c089909cc46de379649582345c6af
Volodymyr Kryvytskyi <VolodymyrKryvytskyi@my.uopeople.edu>
2024-02-09 19:34:37 +0900

    Create a currency selection screen

---
+++ LOTR Converter/SelectCurrency.swift
@@ -0,0 +1,57 @@
+//
+//  SelectCurrency.swift
+//  LOTR Converter
+//
+//  Created by Volodymyr Kryvytskyi on 09.02.2024.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            //Background
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                //Text
+                Text("Select the currency you are starting with:")
+                    .fontWeight(.bold)
+                
+                //Icons
+//                LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+//
+//                }
+                
+                //Text
+                Text("Select the currency you would like to convert to:")
+                    .fontWeight(.bold)
+                    
+                
+                //Icons
+                
+                //Buttom
+                Button("Done") {
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+        
+    }
+}
+
+#Preview {
+    SelectCurrency()
+}
