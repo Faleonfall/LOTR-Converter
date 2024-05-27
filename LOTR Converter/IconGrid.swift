021ec826973b06a751dd5dbb810c387b76b02c40
Volodymyr Kryvytskyi <VolodymyrKryvytskyi@my.uopeople.edu>
2024-05-27 22:12:12 +0200

    Move some logic to the IconGrid view

---
+++ LOTR Converter/IconGrid.swift
@@ -0,0 +1,18 @@
+//
+//  IconGrid.swift
+//  LOTR Converter
+//
+//  Created by Volodymyr Kryvytskyi on 13.05.2024.
+//
+
+import SwiftUI
+
+struct IconGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconGrid()
+}
