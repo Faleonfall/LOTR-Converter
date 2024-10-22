db48379426be51aaa07a4cd03997c6b8f42a845c
Volodymyr Kryvytskyi <faleonfall@gmail.com>
2024-10-22 12:02:33 +0200

    Added convert function

--- LOTR Converter/ContentView.swift
+++ LOTR Converter/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     @State var leftAmount = ""
     @State var rightAmount = ""
     
+    @FocusState var leftTyping
+    @FocusState var rightTyping
+    
     @State var leftCurrency: Currency = .silverPiece
     @State var rightCurrency: Currency = .goldPiece
     
@@ -61,6 +64,9 @@ struct ContentView: View {
                         //Textfield
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
+                            .focused($leftTyping)
+                            .keyboardType(.decimalPad)
+                        
                     }
                     
                     //Equal sign
@@ -94,6 +100,8 @@ struct ContentView: View {
                         TextField("Amount", text: $rightAmount)
                             .textFieldStyle(.roundedBorder)
                             .multilineTextAlignment(.trailing)
+                            .focused($rightTyping)
+                            .keyboardType(.decimalPad)
                         
                     }
                 }
@@ -118,7 +126,22 @@ struct ContentView: View {
                 }
             }
         }
-        //.border(.blue)
+        .onChange(of: leftAmount) {
+            if leftTyping {
+                rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
+            }
+        }
+        .onChange(of: rightAmount) {
+            if rightTyping {
+                leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+            }
+        }
+        .onChange(of: leftCurrency, {
+            rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
+        })
+        .onChange(of: rightCurrency, {
+            leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+        })
         .sheet(isPresented: $showExchangeInfo) {
             ExchangeInfo()
         }
